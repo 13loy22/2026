334f4b868afbdfde897f4ed6d5e33367a0156cb4
loy <loinaz.lichtenstein@gmail.com>
2026-06-21 15:07:48 +0300

    excersices

---
+++ Week1/Day2/ExerciseXP/exercise3.py
@@ -0,0 +1,19 @@
+#Exercise 3: What is the output?
+##Instructions
+#Predict the output of the following code snippets:
+#Coment what is your guess, then run the code and compare
+
+#example:
+
+#>>> 15 < 8 #False
+#>>> 5 < 3
+#>>> 3 == 3
+#>>> 3 == "3"
+#>>> "3" > 3
+#>>> "Hello" == "hello"
+
+print(5 < 3)              # my guess: false
+print(3 == 3)             # my guess: true
+print(3 == "3")           # my guess: false
+# print("3" > 3)          # my guess: type-error 
+print("Hello" == "hello") # my guess: false
---
+++ Week1/Day2/ExerciseXP/exercise8.py
@@ -0,0 +1,13 @@
+# Exercise 8: What’s your name?
+#Instructions
+#Write code that asks the user for their name and determines whether or not you have the same name. Print out a funny message based on the outcome.
+
+
+name = input("What is your name? ")
+
+if name == "Loinaz":
+    print("Wow! We have the same name!")
+else:
+    print("Nice to meet you, but your name is not as weird as mine!")
+
+
