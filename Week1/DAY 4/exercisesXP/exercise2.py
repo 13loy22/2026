fd3e5301d4aa351eec400ca1569c9f6616494ba6
loy <loinaz.lichtenstein@gmail.com>
2026-06-21 23:16:23 +0300

    commmit

---
+++ Week1/DAY 4/exercisesXP/exercise2.py
@@ -0,0 +1,20 @@
+# Exercise 2: Tuple
+#Key Python Topics:
+#Tuples (immutability)
+#Instructions:
+#Given a tuple of integers, try to add more integers to the tuple.
+#Hint: Tuples are immutable, meaning they cannot be changed after creation. Think about why you can’t add more integers to a tuple.
+
+
+# Exercise 2: Tuple
+
+my_tuple = (1, 2, 3)
+
+# Tuples are immutable, so we cannot add items directly.
+
+
+#Could this be the right answer also?
+#print(my_tuple)
+#my_tuple = (1, 2, 3)
+#my_tuple += (4,)
+#print(my_tuple)
