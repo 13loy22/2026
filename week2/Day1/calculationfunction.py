cf449045343a9f9e9a1ca7ffcbe2797a600e7cbe
loy <loinaz.lichtenstein@gmail.com>
2026-06-28 17:20:20 +0300

    w

---
+++ week2/Day1/calculationfunction.py
@@ -0,0 +1,26 @@
+
+def calculation(a, b):
+    addition = a + b
+    subtraction = a - b
+    return addition, subtraction
+
+res = calculation(40, 10)
+print(res)
+
+#if we want 2 values separately
+#def calculation(a, b):
+  #  addition = a + b
+   # subtraction = a - b
+    #return addition, subtraction
+
+#add, sub = calculation(40, 10)
+
+#print(add)
+#print(sub)
+
+#
+def calculation (a,b):
+    return a+b, a-b
+res= calculation(40,10)
+print(res)
+
